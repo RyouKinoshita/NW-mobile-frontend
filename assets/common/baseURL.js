42c1df6bb967ec7ef828cec7b0cc86cf506e4f1c
itsk2 <codonkellyn3@gmail.com>
2024-11-23 09:37:15 +0800

    Dp

--- assets/common/baseURL.js
+++ assets/common/baseURL.js
@@ -6,12 +6,20 @@ let baseURL = '';
 
 {Platform.OS == 'android'
 
-? baseURL = 'http://192.168.1.57:8080/api/v1'
-: baseURL = 'http://192.168.1.57:8080/api/v1'
+// ? baseURL = 'http://192.168.1.57:8080/api/v1'
+// : baseURL = 'http://192.168.1.57:8080/api/v1'
+
+
+// ? baseURL = 'http://172.20.10.2:8080/api/v1'
+// : baseURL = 'http://172.20.10.2:8080/api/v1'
 
 // ? baseURL = 'http://192.168.55.106:8080/api/v1'
 // : baseURL = 'http://192.168.55.106:8080/api/v1'
 
-}
+// ? baseURL = 'http://192.168.69.104:8080/api/v1'
+// : baseURL = 'http://192.168.69.104:8080/api/v1'
 
+? baseURL = 'https://nw-mobile-backend.onrender.com/api/v1'
+: baseURL = 'https://nw-mobile-backend.onrender.com/api/v1'
+}
 export default baseURL;
