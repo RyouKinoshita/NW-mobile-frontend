d641d3dc137671e17b0cd72e9907cf29fc6887a3
RyouKinoshita <ryoukinoshita@gmail.com>
2025-02-05 02:54:45 +0800

    deployment preparations

--- assets/common/baseURL.js
+++ assets/common/baseURL.js
@@ -1,25 +1,22 @@
-import { Platform } from 'react-native'
+import { Platform } from "react-native";
 
+let baseURL = "";
 
+{
+  Platform.OS == "android"
+    ? // ? baseURL = 'http://192.168.1.57:8080/api/v1'
+      // : baseURL = 'http://192.168.1.57:8080/api/v1'
 
-let baseURL = '';
+      // ? baseURL = 'http://172.20.10.2:8080/api/v1'
+      // : baseURL = 'http://172.20.10.2:8080/api/v1'
 
-{Platform.OS == 'android'
+      // ? baseURL = 'http://192.168.55.106:8080/api/v1'
+      // : baseURL = 'http://192.168.55.106:8080/api/v1'
 
-// ? baseURL = 'http://192.168.1.57:8080/api/v1'
-// : baseURL = 'http://192.168.1.57:8080/api/v1'
+      (baseURL = "https://nw-mobile-backend-vomk.onrender.com/api/v1")
+    : (baseURL = "https://nw-mobile-backend-vomk.onrender.com/api/v1");
 
-
-// ? baseURL = 'http://172.20.10.2:8080/api/v1'
-// : baseURL = 'http://172.20.10.2:8080/api/v1'
-
-// ? baseURL = 'http://192.168.55.106:8080/api/v1'
-// : baseURL = 'http://192.168.55.106:8080/api/v1'
-
-? baseURL = 'http://192.168.100.4:8080/api/v1'
-: baseURL = 'http://192.168.100.4:8080/api/v1'
-
-// ? baseURL = 'https://nw-mobile-backend.onrender.com/api/v1'
-// : baseURL = 'https://nw-mobile-backend.onrender.com/api/v1'
+  // ? baseURL = 'https://nw-mobile-backend.onrender.com/api/v1'
+  // : baseURL = 'https://nw-mobile-backend.onrender.com/api/v1'
 }
 export default baseURL;
